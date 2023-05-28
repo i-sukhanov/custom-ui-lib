292e5ce37233207b5c2756d1ccf8760c34db3f20
Ivan Sukhanov <sukhanov12@yandex.ru>
2023-05-28 23:42:28 +0400

    Added checkbox component

---
+++ src/components/checkbox/CheckboxC.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="checkbox">
+    <input
+      class="checkbox--input"
+      type="checkbox"
+      :name="name"
+      :id="id"
+      v-model="val"
+    />
+    <label class="checkbox--label" :for="id">{{ label }}</label>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { computed } from 'vue';
+
+const emit = defineEmits(['change']);
+const props = defineProps({
+  id: {
+    type: String,
+    required: true,
+  },
+  label: {
+    type: String,
+    default: 'checkbox',
+  },
+  name: {
+    type: String,
+  },
+  value: {
+    type: [Boolean, String],
+    default: false,
+  },
+});
+
+const val = computed({
+  get() {
+    return props.value;
+  },
+
+  set(value) {
+    emit('change', value);
+  },
+});
+</script>
+
+<style scoped>
+@import '../../assets/components/checkbox/checkbox.css';
+</style>
---
+++ src/stories/CheckboxC.story.vue
@@ -0,0 +1,9 @@
+<template>
+  <Story>
+    <CheckboxC />
+  </Story>
+</template>
+
+<script setup lang="ts">
+import CheckboxC from '../components/checkbox/CheckboxC.vue';
+</script>
